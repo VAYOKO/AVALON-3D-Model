9af82d0362ced7956ffc0a35675c749f7d228b05
vipa varis <63534193+VAYOKO@users.noreply.github.com>
2026-09-22 20:21:11 +0700

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,8 @@ const PRODUCTS = [
         name: "PID Ball and Beam kit", 
         price: 1499, 
         images: [
-            "pidv2.png"
+            "pidv2.png",
+            "ballandbeam1.png"
         ],
         shortDesc: "ชุดคิทสำหรับเรียนรู้ระบบควบคุม PID พร้อมเอกสารเเละคู่มือการเรียน",
         fullDesc: `ประกอบด้วย ESP32 Servo motor สายจั้ม ชิ้นส่วน 3D print
